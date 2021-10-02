9a2aa7a82abe07a644b6a268926e42c360aa910e
UnicusStella <unicusstella@gmail.com>
2021-10-02 14:00:45 +0900

    modify

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React, { useEffect } from 'react';
-import {
-  Switch,
-  Route,
-  useLocation
-} from 'react-router-dom';
+import { Switch, Route, useLocation } from 'react-router-dom';
 
 import './css/style.scss';
 
@@ -16,7 +12,6 @@ import SignUp from './pages/SignUp';
 import ResetPassword from './pages/ResetPassword';
 
 function App() {
-
   const location = useLocation();
 
   useEffect(() => {
@@ -29,9 +24,9 @@ function App() {
   });
 
   useEffect(() => {
-    document.querySelector('html').style.scrollBehavior = 'auto'
-    window.scroll({ top: 0 })
-    document.querySelector('html').style.scrollBehavior = ''
+    document.querySelector('html').style.scrollBehavior = 'auto';
+    window.scroll({ top: 0 });
+    document.querySelector('html').style.scrollBehavior = '';
     focusHandling('outline');
   }, [location.pathname]); // triggered on route change
 
